7224aeae79d67b9482be05db39b9e21e8c45e200
CrystalGrape <2424994234@qq.com>
2017-03-15 17:30:41 +0800

    定义pn语言格式

--- src/program-n/Program.cs
+++ src/program-n/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace program_n
@@ -9,20 +10,53 @@ namespace program_n
     public class Program
     {
         private static int DataStackBaseAddr = 300;
+        
+        //流程控制列表
+        private static string[] keyword_process = { "if", "else", "for", "while" };
+        //类型关键字列表
+        private static string[] keyword_type = { "int", "double", "char", "var", "int*", "double*", "char*", "var*" };
+        //其它关键字
+        private static string[] keyword_other = { "return", "{", "}" };
+        //关键字列表
+        private static List<string> keyword
+        {
+            get
+            {
+                List<string> set = new List<string>();
+                set.AddRange(keyword_process);
+                set.AddRange(keyword_type);
+                set.AddRange(keyword_other);
+                return set;
+            }
+        }
+
         public static void Main(string[] args)
         {
             if(File.Exists("a.asn"))
             {
                 File.Delete("a.asn");
             }
-            OutputObject asnFile = new OutputObject();
-            asnFile.Asn($"mov dptr,{DataStackBaseAddr}");
-            asnFile.Asn($"jmp main");
-            asnFile.Out();
+            FileStream fs = new FileStream("demo.pn", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            List<string> block = new List<string>();            //代码块
+            //Function inputFunc;
+            while(!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                line = line.TrimStart('\t', ' ', '\r', '\n');
+                line = line.TrimEnd('\t', ' ', '\r', '\n');
 
-            Function func = new Function("test");
-            func.FuncPrepare();
-            func.FuncReturn("data");
+            }
+            
+
+            //OutputObject asnFile = new OutputObject();
+            //asnFile.Asn($"mov dptr,{DataStackBaseAddr}");
+            //asnFile.Asn($"jmp main");
+            //asnFile.Out();
+
+            //Function func = new Function("test");
+            //func.FuncPrepare();
+            //func.FuncReturn("data");
         }
     }
 }
